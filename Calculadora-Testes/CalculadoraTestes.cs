6a8e80ccafa1717ccba78dfc6138594512b08c62
KlSantos1 <148875030+KlSantos1@users.noreply.github.com>
2023-10-24 14:31:15 -0300

    Initial commit

---
+++ Calculadora-Testes/CalculadoraTestes.cs
@@ -0,0 +1,69 @@
+using System;
+using Xunit;
+
+namespace Calculadora_Testes
+{
+    public class CalculadoraTests
+    {
+        [Fact(DisplayName = "Teste de Soma")]
+        [Trait("Category","Operadores")]
+        public void TestSomar()
+        {
+            // Arrange
+            int num1 = 5;
+            int num2 = 3;
+
+            // Act
+            int resultado = Calculadora.Calculadora.Somar(num1, num2);
+
+            // Assert
+            Assert.Equal(8, resultado);
+        }
+
+        [Fact(DisplayName = "Teste de Subtração")]
+        [Trait("Category", "Operadores")]
+        public void TestSubtrair()
+        {
+            // Arrange
+            int num1 = 10;
+            int num2 = 4;
+
+            // Act
+            int resultado = Calculadora.Calculadora.Subtrair(num1, num2);
+
+            // Assert
+            Assert.Equal(6, resultado);
+        }
+
+        [Fact(DisplayName = "Teste de Multiplicação")]
+        [Trait("Category", "Operadores")]
+        public void TestMultiplicar()
+        {
+            // Arrange
+            int num1 = 6;
+            int num2 = 7;
+
+            // Act
+            int resultado = Calculadora.Calculadora.Multiplicar(num1, num2);
+
+            // Assert
+            Assert.Equal(42, resultado);
+        }
+
+        [Fact(DisplayName = "Teste de Divisão")]
+        [Trait("Category", "Operadores")]
+        public void TestDividir()
+        {
+            // Arrange
+            int num1 = 20;
+            int num2 = 4;
+
+            // Act
+            int resultado = Calculadora.Calculadora.Dividir(num1, num2);
+
+            // Assert
+            Assert.Equal(5, resultado);
+        }
+    }
+
+}
---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    { 
+
+        public static int Somar(int num1, int num2)
+        {
+
+            int valorSoma = num1 + num2;
+
+            return valorSoma;
+
+        }
+
+        public static int Subtrair(int num1, int num2)
+        {
+
+            int valorSubtracao = num1 - num2;
+
+            return valorSubtracao;
+
+        }
+
+        public static int Multiplicar(int num1, int num2)
+        {
+
+            int valorMultiplicacao = num1 * num2;
+
+            return valorMultiplicacao;
+
+        }
+
+        public static int Dividir(int num1, int num2)
+        {
+
+            int valorDivisao = num1 / num2;
+
+            return valorDivisao;
+
+        }
+
+    }
+}
